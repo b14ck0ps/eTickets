3aa0e8437d3328b92199235120f1d1fe5bb67488
Azran Hossain <mr69751@gmail.com>
2022-12-20 02:26:08 +0600

    modes created

---
+++ eTickets/Data/Enums/MovieCatagory.cs
@@ -0,0 +1,12 @@
+﻿namespace eTickets.Data.Enums
+{
+    public enum MovieCatagory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Horror,
+        Romance,
+        Thriller
+    }
+}
